caa21263a0719948e97cd6c9620784be4eba2d81
golki <antgolik@gmail.com>
2022-10-14 18:06:33 +0300

    finished basic functions

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const multiply = function(firstNumber, secondNumber){
 }
 
 const divide = function(firstNumber, secondNumber) {
+  if (secondNumber == 0) {
+    btnsArray.forEach(element => element.setAttribute('disabled', true));
+    clearBtn.removeAttribute('disabled');
+  }
   return firstNumber/secondNumber;
 }
 
@@ -29,12 +33,56 @@ const operate = function(operator, firstNumber, secondNumber) {
   }
 }
 
+const isDecimal = function(number) {
+  return number * 10000000 % 10000 != 0 
+}
+
 const display = document.querySelector('h2');
 const btns = document.querySelectorAll('button.btns');
 const btnsArray = Array.from(btns);
+const clearBtn = document.getElementById('clearBtn');
 
-clearBtn.addEventListener('click', () => display.textContent = '');
+let temp = '';
+let currentNumber = 0;
+let lastOperation = '+';
+
 btnsArray.forEach(element => element.addEventListener('click', e => {
-  display.textContent += e.target.textContent;
-
+  let currentSign = e.target.textContent;
+  display.textContent += currentSign;
+  if (currentSign == '+' || currentSign == '-' || currentSign == '*' || currentSign == '/') {
+    currentNumber = operate(lastOperation, currentNumber, parseInt(temp));
+    lastOperation = currentSign;
+    temp = '';
+  } 
+  else if (currentSign == '=') {
+    if (temp == '') {
+      display.textContent = '';
+      return;
+    }
+    currentNumber = operate(lastOperation, currentNumber, parseInt(temp));
+    if (isDecimal(currentNumber)) {
+      currentNumber = currentNumber.toFixed(3);
+    }
+    if (currentNumber == 'Infinity') {
+      display.textContent = 'Error';
+      return
+    }
+    display.textContent = currentNumber;
+    lastOperation = '+';
+    temp = '';
+    temp += currentNumber;
+    currentNumber = 0;
+    
+  }
+  else if (currentSign == 'C') {
+    display.textContent = '';
+    temp = '';
+    currentNumber = 0;
+    lastOperation = '+';
+    btnsArray.forEach(element => element.removeAttribute('disabled'));
+  }
+  else {
+    temp += e.target.textContent;
+  }
+  
 }));
